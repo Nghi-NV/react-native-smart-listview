e7dc8402f932698a47eb883a8ebc3a6fd0d0efb1
NghiNV <nghinv@luci.vn>
2018-05-05 12:26:08 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-
+import PullToRefreshListView from 'react-native-smart-pull-to-refresh-listview';
 import SmartListView from './lib/SmartListView';
 import SmartSwipeRow from './lib/SmartSwipeRow';
 
+const PullState = PullToRefreshListView.constants.viewState;
+
 export {
   SmartListView,
-  SmartSwipeRow
+  SmartSwipeRow,
+  PullState
 };
-
